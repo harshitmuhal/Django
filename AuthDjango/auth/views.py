42311d85ec0bc9286df78e091fd1e14df6a42b5f
Harshit Muhal <hrshtmuhal8@gmail.com>
2020-12-07 20:29:32 +0530

    Authorisation in Django

--- AuthDjango/auth/views.py
+++ AuthDjango/auth/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from auth import forms
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate,login
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 def auth_login(request):
@@ -15,7 +15,7 @@ def auth_login(request):
             password=loginform.cleaned_data['password']
             user=authenticate(username=username,password=password)
             if user:
-                # login(request, user) # login() saves the user’s ID in the session, using Django’s session framework.
+                login(request, user) # login() saves the user’s ID in the session, using Django’s session framework.
                 # it is used to attach the user to the current session.
                 return HttpResponseRedirect('/')
             else:
@@ -25,3 +25,12 @@ def auth_login(request):
     'error':error,
     }
     return render(request,'auth/login.html',context=context)
+
+# OR using generic login views
+
+class Login(LoginView):
+    template_name='auth/login.html'
+    redirect_authenticated_user = True
+
+class Logout(LogoutView):
+    pass

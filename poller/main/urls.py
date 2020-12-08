a3b187ee0f596bf5bff3594c42836f84a25875b4
Harshit Muhal <hrshtmuhal8@gmail.com>
2020-12-08 22:19:06 +0530

    poller changed

--- poller/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from main import views
-
-urlpatterns=[
-    path('',views.index.as_view(),name='question_page'),
-    path('question/<slug>',views.question_detail.as_view(),name='question'),
-]
